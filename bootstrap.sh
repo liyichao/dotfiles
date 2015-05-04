b6f0c246ebd19b57254abfbfb1ff218515ada100
Li Yichao <lyc@zhihu.com>
2015-05-04 18:24:35 +0800

    Move Mac spec to extra.

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,13 +10,16 @@ function doIt() {
 	source ~/.zprofile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
+doIt;
 unset doIt;
+
+# if [ "$1" == "--force" -o "$1" == "-f" ]; then
+# 	doIt;
+# else
+# 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+# 	echo "";
+# 	if [[ $REPLY =~ ^[Yy]$ ]]; then
+# 		doIt;
+# 	fi;
+# fi;
+# unset doIt;
